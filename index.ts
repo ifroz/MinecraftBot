95a154d4f4ea86154a7c59db09cb69ca60259ed4
Laszlo Szucs <ifrozen@gmail.com>
2022-02-04 12:17:00 +0100

    mineflayer.createBot().on('login')

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
+import mineflayer from 'mineflayer'
+import config from './config'
+
 async function main() {
-  console.log("Entry")
+  const bot = mineflayer.createBot({
+    host: config.host,
+    username: config.user,
+    port: config.port,
+  } as any)
+
+  bot.on('login', () => {
+    bot.chat('Hello, my dear humans. ')
+  })
+
+  // bot.on('message', chatMessage => {})
 }
 
 main()
