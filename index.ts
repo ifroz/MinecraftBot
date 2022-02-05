1edf4258c3086e0d8c51a354212f5f64735b7262
Laszlo Szucs <ifrozen@gmail.com>
2022-02-05 14:10:33 +0100

    Implement chat commands through rxjs

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 /// <reference types="./typings" />
 
-import mineflayer, { Bot } from 'mineflayer'
-import { pathfinder } from "mineflayer-pathfinder"
+import mineflayer, { Bot, BotEvents } from 'mineflayer'
+import { pathfinder } from 'mineflayer-pathfinder'
 import { mineflayer as prismarineViewer } from 'prismarine-viewer'
 
-import { first, fromEvent } from 'rxjs'
+import { filter, first, fromEvent } from 'rxjs'
 
 import { followPlayer } from './actions/follow'
 import { sleep } from './actions/sleep'
@@ -25,37 +25,40 @@ async function main() {
   fromEvent(bot, 'spawn')
     .pipe(first())
     .subscribe(async () => {
-        console.log('Spawned.')
-        // prismarineViewer(bot, { firstPerson: true, port: 3333 })
+      console.log('Spawned.')
+      prismarineViewer(bot, { firstPerson: true, port: 3333 })
 
-        onChatMessage(bot, '#help', (username) => bot.whisper(username, HELP))
-        onChatMessage(bot, '#quit', () => bot.quit())
-        onChatMessage(bot, '#sleep', async (user) => {
-          bot.whisper(user, await sleep(bot))
-        })
+      bot.chat(HELP)
 
-        onChatMessage(bot, '#follow', (username) => followPlayer(bot, username))
-        onChatMessage(bot, '#stop', () => bot.pathfinder.stop())
-
-        bot.chat(HELP)
-
-        bot.on('playerJoined', (player) => {
-          bot.whisper(player.username, HELP)
-        })
+      bot.on('playerJoined', (player) => {
+        bot.whisper(player.username, HELP)
       })
-}
+    })
 
-type Action = (username: string) => void | Promise<void>
-function onChatMessage(bot: Bot, message: string, action: Action) {
-  bot.on('chat', async (username, chatMessage) => {
-    if (chatMessage.toString().includes(message)) {
-      try {
-        await action(username)
-      } catch (err) {
-        bot.chat(`Failed to ${message}: ${(err as Error)?.message}`)
-      }
-    }
+  const omitChatMessagesFromUser = (omittedUsername: string) =>
+    filter<Parameters<BotEvents['chat']>>(
+      ([username]) => username !== omittedUsername
+    )
+  const filterChatCommand = (commandString: string) =>
+    filter<Parameters<BotEvents['chat']>>(([, message]) =>
+      message.includes(commandString)
+    )
+
+  const chatFeed = fromEvent<Parameters<BotEvents['chat']>>(bot, 'chat')
+  const incomingChatFeed = chatFeed.pipe(omitChatMessagesFromUser(bot.username))
+  const onChatMessage = (
+    chatCommand: string,
+    action: (args: Parameters<BotEvents['chat']>) => void
+  ) => incomingChatFeed.pipe(filterChatCommand(chatCommand)).subscribe(action)
+
+  onChatMessage('#help', ([username]) => bot.whisper(username, HELP))
+  onChatMessage('#quit', () => bot.quit())
+  onChatMessage('#sleep', async ([username]) => {
+    bot.whisper(username, await sleep(bot))
   })
+
+  onChatMessage('#follow', ([username]) => followPlayer(bot, username))
+  onChatMessage('#stop', () => bot.pathfinder.stop())
 }
 
 main()
